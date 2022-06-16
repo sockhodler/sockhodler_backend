5617c9872bbd22014e55faf4dd8187178078737a
wxcocoxw <xluxaeternax@protonmail.com>
2022-06-16 11:06:49 -0400

    typo?

--- src/app.module.ts
+++ src/app.module.ts
@@ -15,7 +15,7 @@ import { EmailModule } from './modules/email/email.module';
     }),
     TypeOrmModule.forRoot({
       type: 'mysql',
-      host: process.env.MYSQL_HOST || 'locahost',
+      host: process.env.MYSQL_HOST || 'localhost',
       port: Number(process.env.MYSQL_PORT) || 3306,
       username: process.env.MYSQL_USER,
       password: process.env.MYSQL_PASSWORD,
